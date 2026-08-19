3f728d8ec4fbf7676b8ddf7b647763606e6592f0
R SAI REVANTH <revanthrachapudi8@gmail.com>
2026-08-19 22:26:50 +0530

    in it

---
+++ day8.py
@@ -0,0 +1,104 @@
+#write the outputs before executing
+#TASK 1
+n = 9
+if n % 3 == 0:
+    print('A')
+print('Outside')
+'''
+if 9 is divisible by 3 then it will print A  and else it will print outside
+ n=9
+if n%3==0
+print('A')
+else:
+    print('outside')
+Write Output:
+A
+Outside
+'''
+
+#TASK 2:
+n = 10
+if n % 10 == 0:
+    print('A')
+if n % 5 == 0:
+    print('B')
+print('Outside')
+'''
+if 10 is divisible by 10 then it will print A and if 10 is divisible by 5 then it will print B and else it print outside
+n=10
+if n%10==0
+print('A')
+if n%5==0
+print('B')
+else:
+print('outside)
+Write Output:
+A
+B
+Outside
+'''
+
+#TASK 3:
+n = 10 
+if n % 10 == 0:
+    print('A')
+elif n % 5 == 0:
+    print('B')
+print('Outside')
+'''
+if 10 is divisible by 10 then it will print A and  elif if 10 is divisible by 5 it will print B and  else it will print outside
+n = 10 
+if n % 10 == 0:
+    print('A')
+elif n % 5 == 0:
+    print('B')
+    else:
+print('Outside')
+Write Output:
+A
+B 
+Outside
+''
+#TASK 4:
+n = 10 
+if n % 6 == 0:
+    print('A')
+elif n % 3 == 0:
+    print('B')
+else:
+    print('C')
+print('Outside')
+'''
+n = 10 
+if n % 6 == 0:
+    #10 IS DIVISBLE BY6 
+    print('A')
+    #10 IS NOT DIVISIBLE BY 6
+    print('c')
+elif n % 3 == 0:
+    print('B')
+else:
+    print('C')
+print('outside')
+''
+'c'
+'outside'
+#TASK 5:
+marks = 89 
+if marks > 40:
+    if marks > 75:
+        print('Dictinction') 
+    else:
+        print('Pass')
+else:
+    print('Fail')
+    ''''''
+marks = 89 
+if marks > 40:
+    if marks > 75:
+        print('Dictinction') 
+    else:
+        print('Pass')
+else:
+    print('Fail')
+    "Dictination"
